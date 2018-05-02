2077b141207ef04b5c4a62dca92501e4bc3ff876
YUDAW\yudaw <yudaw@icloud.com>
2018-05-02 14:59:20 +0900

    commonのモデルを作成しました。 #12

--- common/models.py
+++ common/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class App(models.Model):
+    app_id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    logo = models.URLField(null=True)
+    summary = models.TextField(max_length=1000, null=True)
+
+    def __str__(self):
+        return str(self.pk)
