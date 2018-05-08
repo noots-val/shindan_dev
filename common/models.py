6a46fbbdf7da440898a85061eb62af3e1652dc34
YUDAW\yudaw <yudaw@icloud.com>
2018-05-08 22:43:33 +0900

    commonのモデルを編集しました。 __str__～～は「文字列に興すため」に使ってます。 #12

--- common/models.py
+++ common/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class App(models.Model):
-    app_id = models.AutoField(primary_key=True)
-    category = models.CharField(max_length=50)
-    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     logo = models.URLField(null=True)
-    summary = models.TextField(max_length=1000, null=True)
+    summary = models.TextField(null=True)
 
     def __str__(self):
         return str(self.pk)
