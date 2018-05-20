92b46d3b62f81fe102d64bdc24116640e41a6bdf
YUDAW\yudaw <yudaw@icloud.com>
2018-05-20 21:56:07 +0900

    commonのadminを編集しました #22

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,9 @@
+from .models import App
 from django.contrib import admin
 
-# Register your models here.
+
+class AppAdmin(admin.ModelAdmin):
+    fields = ["id", "category", "title", "logo", "summary"]
+
+
+admin.site.register(App, AppAdmin)
