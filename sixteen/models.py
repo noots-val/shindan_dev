d774807a9b55cbb941e949b8e85278f20904e17e
noots-val <noots.val@gmail.com>
2018-06-28 22:22:38 +0900

    jsの修正→typeテーブルにimgカラムを追加、表示

--- sixteen/models.py
+++ sixteen/models.py
@@ -14,6 +14,7 @@ from django.db import models
 class Type(models.Model):
     name = models.CharField(max_length=4)
     description = models.TextField()
+    img = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return str(self.pk)
