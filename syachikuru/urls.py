f1279ad60becab5311d06cb0f8480db27f6ed833
YUDAW\yudaw <yudaw@icloud.com>
2018-07-09 22:13:36 +0900

    確認用枝 編集したadmin, models,urls,viewsをコミット

--- syachikuru/urls.py
+++ syachikuru/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import QuestionsView, ResultView
 
 
 app_name = 'syachikuru'
 urlpatterns = [
-    path('questions/', QuestionsView.as_view(), name='questions'),
-    path('result/pk/', ResultView.as_view(), name='result'),
 ]
