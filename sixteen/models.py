1b1029cc114ebfb55088ba8645b13d29d8c12698
YUDAW\yudaw <yudaw@icloud.com>
2018-07-09 22:55:04 +0900

    確認用枝

--- sixteen/models.py
+++ sixteen/models.py
@@ -7,3 +7,13 @@ from django.db import models
 1. 16タイプER図.a5er
 2. 16タイプテーブル定義書.xlsx（ツールで出力可能）
 """
+
+
+class PokemonResult(models.Model):
+    name = models.CharField(max_length=255)
+    head = models.TextField()
+    description = models.TextField()
+    img = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.pk)
