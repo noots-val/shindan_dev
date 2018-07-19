2c7b8d7d4f18e4a6a03d63978a9d5e9ebf58344e
YUDAW\yudaw <yudaw@icloud.com>
2018-07-19 22:24:51 +0900

    新たに書き加えた部分がかなりあるのでとりあえず上げます #68_社畜るアプリの実装

--- syachikuru/urls.py
+++ syachikuru/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import QuestionsView, ResultView
+from .views import QuestionsView, ResultView, answer
 
 
 app_name = 'syachikuru'
 urlpatterns = [
     path('questions/', QuestionsView.as_view(), name='questions'),
+    path('answer/', answer, name='answer'),
     path('result/pk/', ResultView.as_view(), name='result'),
 ]
--- syachikuru/views.py
+++ syachikuru/views.py
@@ -1,5 +1,7 @@
-from .models import Question, Choice
-from django.views.generic import ListView
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Question, Choice, Result, Characteristic
+from django.views.generic import ListView, DetailView
+from django.urls import reverse
 
 
 class QuestionsView(ListView):
@@ -12,11 +14,6 @@ class QuestionsView(ListView):
     def get_context_data(self, **kwargs):
         """
         QuestionテーブルとChoiceテーブルによって、実際に表示するためのリストを作成する
-
-        テーブルを結合せずに冗長なループを行う理由は、
-        「質問文 [選択肢1 選択肢2 選択肢3 選択肢4 選択肢5]」というデータの持ち方を実現するため。
-        結合させつつ、上記の形で実装する方法があるならばそちらへと変更する
-
         :return: context
         """
         context = super().get_context_data(**kwargs)
@@ -25,17 +22,125 @@ class QuestionsView(ListView):
 
         display_question_list = []
         for question in questions:
-            choice_sentences = [choice.choice_sentence for choice in choices if choice.question.pk == question.pk]
+            choices_of_question = [choice for choice in choices if choice.question.pk == question.pk]
 
             question_dict = {"question_id": question.pk,
                              "question_sentence": question.question_sentence,
-                             "choice_sentences": choice_sentences}
+                             "choices_of_question": choices_of_question}
             display_question_list.append(question_dict)
 
         context["display_question_list"] = display_question_list
         return context
 
 
-class ResultView(ListView):
-    model = Question
+def answer(request):
+    if request.POST is not None:
+        characteristics = Question.objects.values_list('characteristic', flat=True)  # 各質問文のタイプ分けid
+        characteristic_types = Characteristic.objects.values_list('characteristic_type', flat=True)  # 各タイプ（5種類）
+
+        # 各質問文に対応した点数のリストanswersを作成
+        answers = []
+        for characteristic in range(len(characteristics)):    # POSTデータの取得と得点への変換
+            radio_value = int(request.POST["radio" + str(characteristic + 1)])
+            point_value = 5 - ((radio_value - 1) % 5)  # point_valueは実際の得点の値:1～5
+            answers.append(point_value)
+
+        # 各タイプの得点characteristic_type_pointsを計算
+        characteristic_type_points = [0 for i in range(len(characteristic_types))]  # リストの初期化
+        for answer in range(len(characteristics)):
+            for characteristic_type in range(len(characteristic_types)):
+                if characteristics[answer] == characteristic_type + 1:
+                    characteristic_type_points[characteristic_type] += answers[answer]
+                    break
+
+        # 合計点数ごとにresult_idを選択
+        sum_point = sum(characteristic_type_points)
+        if sum_point > 75:
+            result_id = 1
+        elif sum_point > 50:
+            result_id = 2
+        elif sum_point > 25:
+            result_id = 3
+        else:
+            result_id = 4
+
+        response = HttpResponseRedirect(reverse('syachikuru:result', kwargs={'pk': result_id}))
+        response.set_cookie('PEACOCKERY', characteristic_type_points[0])
+        response.set_cookie('LOYALTIES', characteristic_type_points[1])
+        response.set_cookie('ADMISSIBILITY', characteristic_type_points[2])
+        response.set_cookie('RESPONSIBILITY', characteristic_type_points[3])
+        response.set_cookie('COOPERATIVENESS', characteristic_type_points[4])
+    else:
+        return HttpResponseRedirect(reverse('syachikuru:questions'))
+
+
+class ResultView(DetailView):
+    model = Result
     template_name = 'syachikuru/result.html'
+
+    def draw_fig(request):
+        from math import pi
+        import matplotlib.pyplot as plt
+        from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+        plt.rcParams['font.family'] = 'IPAPGothic'  # 全体のフォントを設定
+
+        characteristic_types = Characteristic.objects.values_list('characteristic_type', flat=True)
+        characteristic_type_points = []
+        characteristic_type_points[0] = request.COOKIES['PEACOCKERY']
+        characteristic_type_points[1] = request.COOKIES['LOYALTIES']
+        characteristic_type_points[2] = request.COOKIES['ADMISSIBILITY']
+        characteristic_type_points[3] = request.COOKIES['RESPONSIBILITY']
+        characteristic_type_points[4] = request.COOKIES['COOPERATIVENESS']
+
+        # 軸の作成と設定
+        N = len(characteristic_types)  # 軸数の決定
+        x_as = [2 * pi * n / float(N) for n in range(N)]
+        characteristic_type_points += characteristic_type_points[:1]
+        x_as += x_as[:1]
+
+        plt.rc('axes', linewidth=0.5, edgecolor="#888888")  # 軸の位置とカラー
+        # ax = plt.figure().add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
+        fig = plt.figure(1, figsize=(5, 5))
+        ax = fig.add_subplot(111, polar=True)
+        ax.set_theta_offset(pi / 2)  # 0点を上側に設定
+        ax.set_theta_direction(-1)  # 基本は反時計回りなので、時計回りに変更
+        ax.set_rlabel_position(0)  # 軸の値をどこに定めるか（今回なら上側）
+        ax.xaxis.grid(True, color="#888888", linestyle='solid', linewidth=0.5)
+        ax.yaxis.grid(False, color="#888888", linestyle='solid', linewidth=0.5)
+        plt.xticks(x_as[:-1], [])  # 「何度」の表示を消すため（x軸の値（動径方向））
+        plt.yticks([5, 10, 15, 20, 25], ["20%", "40%", "60%", "80%", "100%"])  # 値の範囲設定・・・pointの構造によりけりなのであとで変数で編集
+
+        # グラフの見た目の設定
+        ax.plot(x_as, characteristic_type_points, linewidth=0, linestyle='solid', zorder=1)
+        ax.fill(x_as, characteristic_type_points, facecolor="#4EE5DA", alpha=1)
+
+        # 動径方向の範囲設定・・・pointの構造によりけりなのであとで変数で編集
+        plt.ylim(0, 25)
+
+        # ラベルの位置の設定
+        for i in range(N):
+            angle_rad = i / float(N) * 2 * pi
+
+            if angle_rad == 0:
+                ha, distance_ax = "center", 3
+            elif 0 < angle_rad < pi:
+                ha, distance_ax = "left", 2
+            elif angle_rad == pi:
+                ha, distance_ax = "center", 2
+            else:
+                ha, distance_ax = "right", 2
+
+            ax.text(angle_rad, 25 + distance_ax, characteristic_types[i], size=10,
+                    horizontalalignment=ha, verticalalignment="center")
+
+        canvas = FigureCanvas(fig)
+        response = HttpResponse(content_type='image/png')
+        canvas.print_png(response)
+        return response
+
+    """
+        import logging
+        for radio_number in range(25):
+            logging.debug(request.POST["radio" + str(radio_number)])
+    """
