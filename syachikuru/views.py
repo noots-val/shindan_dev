e37ac5baf9556ec4aaf78d6942d53cdd9431399a
YUDAW\yudaw <yudaw@icloud.com>
2018-07-24 22:36:55 +0900

    社畜るviewsを編集(answerとresult部分) 102行目まではマジックナンバーをかなり減らしました 減らそうと思えばanswerのresult_idを決定する部分とかももちろんできますが 式がかなり膨らみそうなのでとりあえず保留してます。 #68_社畜るアプリの実装

--- syachikuru/views.py
+++ syachikuru/views.py
@@ -4,6 +4,9 @@ from .services import CreateQuestionListService, CalculatePointService
 from .values import ResultTypeValue
 from django.views.generic import ListView, DetailView
 from django.urls import reverse
+from math import pi
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 import logging
 
@@ -35,27 +38,18 @@ class ResultView(DetailView):
     def get_context_data(self, **kwargs):
         logging.debug(self.request.session['point_dict'])
 
-    def draw_fig(self, request):
-        from math import pi
-        import matplotlib.pyplot as plt
-        from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-
+    def draw_fig(request):
         plt.rcParams['font.family'] = 'IPAPGothic'  # 全体のフォントを設定
 
         characteristic_types = Characteristic.objects.values_list('characteristic_type', flat=True)
+        characteristic_types_length = len(characteristic_types)  # タイプの数
         characteristic_type_points = []
-        characteristic_type_points[0] = request.session['peacockery_point']
-        characteristic_type_points[1] = request.session['loyalties_point']
-        characteristic_type_points[2] = request.session['admissibility_point']
-        characteristic_type_points[3] = request.session['responsibility_point']
-        characteristic_type_points[4] = request.session['cooperativeness_point']
-        logging.debug(characteristic_type_points)
 
         # 軸の作成と設定
-        N = len(characteristic_types)  # 軸数の決定
-        x_as = [2 * pi * n / float(N) for n in range(N)]
+        number_of_ax = len(characteristic_types)  # 軸数の決定
+        angle_of_ax = [2 * pi * n / float(number_of_ax) for n in range(number_of_ax)]  # 各軸の角度を計算し値を代入（下2行も併せて）
         characteristic_type_points += characteristic_type_points[:1]
-        x_as += x_as[:1]
+        angle_of_ax += angle_of_ax[:1]
 
         plt.rc('axes', linewidth=0.5, edgecolor="#888888")  # 軸の位置とカラー
         # ax = plt.figure().add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
@@ -66,19 +60,19 @@ class ResultView(DetailView):
         ax.set_rlabel_position(0)  # 軸の値をどこに定めるか（今回なら上側）
         ax.xaxis.grid(True, color="#888888", linestyle='solid', linewidth=0.5)
         ax.yaxis.grid(False, color="#888888", linestyle='solid', linewidth=0.5)
-        plt.xticks(x_as[:-1], [])  # 「何度」の表示を消すため（x軸の値（動径方向））
+        plt.xticks(angle_of_ax[:-1], [])  # 「何度」の表示を消すため（x軸の値（動径方向））
         plt.yticks([5, 10, 15, 20, 25], ["20%", "40%", "60%", "80%", "100%"])  # 値の範囲設定・・・pointの構造によりけりなのであとで変数で編集
 
         # グラフの見た目の設定
-        ax.plot(x_as, characteristic_type_points, linewidth=0, linestyle='solid', zorder=1)
-        ax.fill(x_as, characteristic_type_points, facecolor="#4EE5DA", alpha=1)
+        ax.plot(angle_of_ax, characteristic_type_points, linewidth=0, linestyle='solid', zorder=1)
+        ax.fill(angle_of_ax, characteristic_type_points, facecolor="#4EE5DA", alpha=1)
 
         # 動径方向の範囲設定・・・pointの構造によりけりなのであとで変数で編集
         plt.ylim(0, 25)
 
         # ラベルの位置の設定
-        for i in range(N):
-            angle_rad = i / float(N) * 2 * pi
+        for i in range(number_of_ax):
+            angle_rad = i / float(number_of_ax) * 2 * pi
 
             if angle_rad == 0:
                 ha, distance_ax = "center", 3
